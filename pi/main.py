1bdd15aebda0be1c57e21508bbab2e171a900c31
girinssh <tngus5851@naver.com>
2021-09-23 20:43:26 +0900

    add main

---
+++ pi/main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 23 18:58:26 2021
+
+@author: tngus
+"""
+
+import lidarManager
+
+def getCommand():
+    pass
+
+def postCommand():
+    pass
+
+def init():
+    pass
+
+def main():
+    pass
