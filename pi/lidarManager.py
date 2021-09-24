2ce6d330a5c3dda9167c940031150ef1db4723c5
girinssh <tngus5851@gmail.com>
2021-09-24 19:17:25 +0900

    Shin: implement getRaws

--- pi/lidarManager.py
+++ pi/lidarManager.py
@@ -5,9 +5,8 @@ Created on Thu Sep 23 16:05:30 2021
 @author: tngus
 """
 
-# import lidar
+import lidar
 import time
-import threading
 
 class LiDARManager:
 
@@ -15,37 +14,47 @@ class LiDARManager:
     POS_RIGHT = 1
     POS_BACK = 2
     
-    
     STATE_FORWARD = 1
     STATE_BACKWARD = -1
     STATES = [STATE_FORWARD, STATE_BACKWARD]
     
-    def __init__(self, rpm, samp_rate):
+    def __init__(self, rpm, samp_rate, minAngle, maxAngle):
         self.rpm = rpm
         self.samp_rate = samp_rate
         
         self.state = LiDARManager.STATE_FORWARD
         
-        # self.lidars = [
-        #         lidar("/dev/serial0", samp_rate=samp_rate),
-        #         lidar("/dev/ttyAMA1", samp_rate=samp_rate),
-        #         lidar("/dev/ttyAMA2", samp_rate=samp_rate)
-        #     ]
+        self.lidars = [
+                lidar("/dev/serial0", samp_rate=samp_rate),
+                lidar("/dev/ttyAMA1", samp_rate=samp_rate),
+                #lidar("/dev/ttyAMA2", samp_rate=samp_rate)
+            ]
         
-        self.distPerOneway = 60 / (self.rpm * 2) * samp_rate
+        # 편도로 가는 동안 몇개의 데이터를 수집해야하는가.
+        self.rawPerOneway = 60 / (self.rpm * 2) * samp_rate
+        
+        # 1개의 데이터를 수집할 때 몇초를 소모해야하는가. 
+        self.secPerRaw = (float)(1 / samp_rate)
+        self.angle_min = minAngle
+        self.angle_max = maxAngle
+        self.angle_range = maxAngle - minAngle
+        self.angle_unit = self.angle_range / self.rawPerOneway
     
-    def getDistances(self, POS: int)->list:
-        distArray = []
-        angleArray = []
-        
-        if POS == LiDARManager.POS_LEFT:
-            pass
-        elif POS == LiDARManager.POS_RIGHT:
-            pass
-        elif POS == LiDARManager.POS_BACK:
-            pass
-        
-        return []
+    def getRaws(self, POS: int)->list:
+        rawArray = [-1]*self.rawPerOneway
+        angleArray = [-1]*self.rawPerOneway
+        start = time.time()
+        index = 0
+        last = -1
+        while index < self.rawPerOneway:
+            last = time.time()
+            #time을 계산하면서 rawArray에 집어넣는다. 
+            if last - start < index * self.secPerRaw:
+                rawArray[index] = self.lidars[POS].read_data()
+                angleArray[index] = self.angle_unit * index + self.angle_min
+            index += 1
+
+        return rawArray, angleArray
 
     def setState(self, newState):
         if newState in LiDARManager.STATES:
