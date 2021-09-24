92371a81643bbe5174f961b86bf13d645cc8fdd3
girinssh <tngus5851@gmail.com>
2021-09-24 20:21:42 +0900

    implement threading and visual test code

--- pi/main.py
+++ pi/main.py
@@ -5,16 +5,45 @@ Created on Thu Sep 23 18:58:26 2021
 @author: tngus
 """
 
-import lidarManager
+from lidarManager import LiDARManager 
+import threading
+import matplotlib.plot as plt
+import numpy as np
 
-def getCommand():
-    pass
+class Main:
 
-def postCommand():
-    pass
+    def getCommand():
+        pass
 
-def init():
-    pass
+    def postCommand():
+        pass
+    
+    def getRPM():
+        return 120
+    
+    
+    def __init__(self):
+        self.lm = LiDARManager(Main.getRPM(), 100, -50, 50)
+        
+    def run(self):
+        rawsAndAngles = 0
+        t = threading.Thread(rawsAndAngles = self.lm.getRaws, args=(0))
+        t.start()
+        
+        t.join()
+        
+        rawList = rawsAndAngles[0]
+        angleList = rawsAndAngles[1]
+        
+        plt.style.use('ggplot') # figure formatting
+        fig,ax = plt.subplots(figsize=(12,9)) # figure and axis
+        ax.plot(angleList ,rawList,linewidth=3.5) # plot ranging data
+        ax.set_ylabel('Distance [m]',fontsize=16) 
+        ax.set_xlabel('Angle [DEG]',fontsize=16)
+        ax.set_title('TF-Luna Ranging Test',fontsize=18)
+        plt.show() # show figure
+        
+        
+main = Main()
 
-def main():
-    pass
+main.run()
