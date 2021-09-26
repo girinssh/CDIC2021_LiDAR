cf64d3e1e6e113b3766de576075b956051dc3135
girinssh <tngus5851@gmail.com>
2021-09-26 18:21:11 +0900

    for test

--- pi/main.py
+++ pi/main.py
@@ -91,9 +91,9 @@ class Main:
         
         plt.style.use('ggplot') # figure formatting
         fig,ax = plt.subplots(figsize=(12,9)) # figure and axis
-        if inlier.size() > 0 :    
+        if inlier.size > 0 :    
             ax.scatter(inlier[:][1],inlier[:][0]) # plot ranging data
-        if outlier.size() > 0 :    
+        if outlier.size > 0 :    
             ax.scatter(outlier[:][1],outlier[:][0]) # plot ranging data
         ax.set_ylabel('Distance [m]',fontsize=16) 
         ax.set_xlabel('Angle [DEG]',fontsize=16)
