cd6dc57428b217fd9085aea07001eb11cd9e1c68
girinssh <tngus5851@gmail.com>
2021-09-26 18:20:22 +0900

    for test

--- pi/main.py
+++ pi/main.py
@@ -91,8 +91,10 @@ class Main:
         
         plt.style.use('ggplot') # figure formatting
         fig,ax = plt.subplots(figsize=(12,9)) # figure and axis
-        ax.scatter(inlier[:][1],inlier[:][0]) # plot ranging data
-        ax.scatter(outlier[:][1],outlier[:][0]) # plot ranging data
+        if inlier.size() > 0 :    
+            ax.scatter(inlier[:][1],inlier[:][0]) # plot ranging data
+        if outlier.size() > 0 :    
+            ax.scatter(outlier[:][1],outlier[:][0]) # plot ranging data
         ax.set_ylabel('Distance [m]',fontsize=16) 
         ax.set_xlabel('Angle [DEG]',fontsize=16)
         ax.set_title('TF-Luna Ranging Test',fontsize=18)
