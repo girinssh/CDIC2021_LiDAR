5f3c61fb10968a8612022d314433fee49c13f2b0
girinssh <tngus5851@gmail.com>
2021-09-29 19:47:52 +0900

    test code

--- pi/pi_method.py
+++ pi/pi_method.py
@@ -7,9 +7,8 @@ def raw2YPOS(pos:int, rawDistList, srvo_ang:float, angleList, velocity: float):
 # 	for i in range (0, len(rawDistList)): 
 # 		distanceList[i] = rawDistList[i]*math.cos(math.radians(srvo_ang))*math.cos(abs(math.radians(angleList[i])-(math.pi/2)))
     # return distanceList
-    
-    yList = rawDistList*np.cos(srvo_ang)*np.sin(angleList)
-
+    # yList = rawDistList*np.cos(srvo_ang)*np.sin(angleList)
+    yList = rawDistList*np.cos(srvo_ang)*np.cos(abs(angleList-np.pi/2))
     return pos, yList
 
 
