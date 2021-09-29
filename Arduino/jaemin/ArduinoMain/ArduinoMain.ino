3be85402b591620e03a1a1d35f8e9464edbef837
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2021-09-30 00:34:47 +0900

    Arduino Main

--- Arduino/jaemin/ArduinoMain/ArduinoMain.ino
+++ Arduino/jaemin/ArduinoMain/ArduinoMain.ino
@@ -1,6 +1,8 @@
 
 
+#define GPSBAUD 9600
 
+Gps gps(GPSBAUD);
 
 void setup() {
   //step_moter init
@@ -11,29 +13,27 @@ void setup() {
 void loop() {
 
   
-  gps_velocity = gps.getVelocity();
-  
+  float gps_velocity = gps.getVelocity();
   //send data(gps_velocity (+ imu(roll, pitch)))
   if(Serial.available() == 0){
-    
+    Serial.println
   }
-
+  
   //waiting & led initial
   while(!Serial.available())(
-    
   }
 
   //recive data
-  String recv_data = Serial.read();
+  String recv_data = Serial.readString();
   //parsing
-
-
+    
+  
   //servo setting
   servo.set_deg(srvo_deg);
   
   //control led
-  if(warn_case){
-    led.set(led_num);
+  if(prev_warn_case != warn_case){
+    led.set(led_num, warn_case);
   }
   
 }
