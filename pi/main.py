cce19459d710ba0f231fc2b4266248f4c4e4acb5
girinssh <tngus5851@gmail.com>
2021-09-29 22:41:39 +0900

    test code

--- pi/main.py
+++ pi/main.py
@@ -130,8 +130,8 @@ class Main:
             
             heightList = heightList.result()
             xposList = xposList.result()
-            print(yposList[0])
             yposList = yposList.result()
+            print(yposList[0])
             
             #print("(Roll, Pitch) = {}".format(self.imu.getRollPitch()))
             
