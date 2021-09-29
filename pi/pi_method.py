417167efa889db6fdd8d278bb0c85ec488e78fbd
girinssh <tngus5851@gmail.com>
2021-09-29 20:52:28 +0900

    test code

--- pi/pi_method.py
+++ pi/pi_method.py
@@ -4,11 +4,12 @@
 import numpy as np
 
 def raw2YPOS(pos:int, rawDistList, srvo_ang:float, angleList, velocity: float):
-# 	for i in range (0, len(rawDistList)): 
-# 		distanceList[i] = rawDistList[i]*math.cos(math.radians(srvo_ang))*math.cos(abs(math.radians(angleList[i])-(math.pi/2)))
-    # return distanceList
-    yList = rawDistList*np.cos(srvo_ang)*np.sin(angleList)
-    return pos, yList
+    
+ 	# for i in range (len(rawDistList)): 
+  #        distanceList[i] = rawDistList[i]*math.cos(math.radians(srvo_ang))*math.cos(abs(math.radians(angleList[i])-(math.pi/2)))
+    return pos, rawDistList*np.cos(srvo_ang)*np.cos(abs(angleList-(np.pi/2)))
+    # yList = rawDistList*np.cos(srvo_ang)*np.sin(angleList)
+    # return pos, yList
 
 
 def raw2XPOS(pos:int, rawDistList, srvo_ang:float, angleList):
