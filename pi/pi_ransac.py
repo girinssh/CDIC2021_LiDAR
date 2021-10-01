a22738f50c5622ae1adc2d1af94ca2c696b3f5ff
plateeb61 <84173786+plateeb61@users.noreply.github.com>
2021-10-02 04:41:08 +0900

    Update pi_ransac.py

--- pi/pi_ransac.py
+++ pi/pi_ransac.py
@@ -1,68 +1,53 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 22 17:41:41 2021
-@author: tngus
-"""
-
 import numpy as np
-# import main
 
 class dangerDetection:
-    MIN_ANGLE = 2*np.pi/9
-    MAX_ANGLE = 7*np.pi/9
-
-    # y expression
-    # def getBase(radius:float):
-    #     Main = main.Main.getInstance()
-    #     return radius, (1 if Main.goLeft else -1) * 9*Main.velocity*Main.onewayTime/(5*np.pi), (-7 if Main.goLeft else 2)*Main.velocity*Main.onewayTime/5
-
-    def getNumberOfCycle(m: int, alpha=0.8, p=0.01)-> int:
-        return int(np.log10(p)/np.log10(1-alpha ** m))
+    #~…
     
-    # def RANSAC(cls, pos:int, pList): #pList [x1, y1], [x2, y2] ... #pList = [angle(radian), distance]
-    #     maxInliers = []
-    #     finOutliers = [] # final outliers list
+    def RANSAC(XPOS, H): #XH 평면을 바라보고
+        #XPOS : 라이다에서 측정 포인트까지의 x축 방향 distance
+        #YPOS :
+        #H = heightList : 지면에서 측정 포인트까지의 높이 #1차원 리스트
         
-    #     print(pList.shape)
+        maxInliers = []
+        finOutliers = [] # final outliers list #[i1, i2, …, in] 인덱스 번호
         
-    #     a = np.sqrt(pList[-1][0] ** 2 + pList[-1][1] ** 2)
-        
-    #     y_th = 0.5 # threshold value [m]
+        # algo rotation num is already set: 14
+        for i in range(0, 14):
+            while True: 
+                i1, i2 = np.random.randint(0, len(XPOS), size=2)
+                if (i1 != i2):
+                    p1 = np.array([XPOS[i1], H[i1]])
+                    p2 = np.array([XPOS[i2], H[i2]])
+                    break
+                # a 계산 시 분모가 0이 되는 걸 방지
 
-    #     # algo rotation num is already set: 14
-    #     for i in range(cls.getNumberOfCycle(pList.shape[0])):
-    #         i1, i2 = np.random.randint(0, pList[:,0].size, size=2)
-            
-    #         while i1 == i2:
-    #             i2 = np.random.randint(0, pList[:,0].size, size=1)
-    #         p1 = pList[i1]
-    #         p2 = pList[i2]
+            #두 점을 지나는 직선 (z=)f(x)=ax+b 구하기 
+            a = (p2[1]-p1[1])/(p2[0]-p1[0])
+            b = p1[1]-a*p1[0]
 
-    #         t1pt2d2 = np.arctan2(p2[0]-p1[0], p1[1]-p2[1])
-            
-
-    #         inliers = []
-    #         outliers = []
+            inliers = []
+            outliers = []
     
-    #         for p in pList:
-    #             # print(p)
-    #             x = p[0] 
-    #             y = p[1] 
+            for i in range(len(XPOS)) :
+                x = XPOS[i] # x value of ith point
+                z = H[i] # height of ith point
 
-    #             # 임계값 넘어가면 outlier 
-    #             # 임계값 내에 있으면 inlier
+                # 임계값 넘어가면 outlier 
+                # 임계값 내에 있으면 inlier
 
-    #             t = np.arccos(x/a)
-    #             py = a * np.sin(t) - b * t + c # p1, p2로 만든 식에 만족하는 y 값
+                z_th = 0.3 # threshold value [m]
 
-    #             if abs(y - py) > y_th:
-    #                 outliers.append([x,y])
-    #             else:
-    #                 inliers.append([x,y])
+                pz = a*x+b # p1, p2로 만든 식에 만족하는 z값
 
-    #         if len(inliers) > len(maxInliers):
-    #             maxInliers = inliers.copy()
-    #             finOutliers = outliers.copy()
-    #             param = np.array([a, b, c], dtype=float)
-    
-    #     return np.array(maxInliers), np.array(finOutliers), param
+                if abs(z - pz) > z_th:
+                    outliers.append([i])
+                else:
+                    inliers.append([i])
+
+            if len(inliers) > len(maxInliers):
+                maxInliers = inliers
+                finOutliers = outliers
+                param = [a, b]
+
+    return maxInliers, finOutliers, param
+
