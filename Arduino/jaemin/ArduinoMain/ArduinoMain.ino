48cd504123952151a39dff785cee1d9eb3cdadc2
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2021-10-01 16:54:05 +0900

    Arduino Main

--- Arduino/jaemin/ArduinoMain/ArduinoMain.ino
+++ Arduino/jaemin/ArduinoMain/ArduinoMain.ino
@@ -3,15 +3,20 @@
 #define GPSBAUD 9600
 
 Gps gps(GPSBAUD);
+int led_spd;
+int led_pre_time = 0, led_cur_time;
+boolean isLedOn = false
 
 void setup() {
+  //led, speaker init
+  ledsp_setup();
+  
   //step_moter init
 
   //servo init deg
 }
 
 void loop() {
-
   
   float gps_velocity = gps.getVelocity();
   //send data(gps_velocity (+ imu(roll, pitch)))
@@ -26,7 +31,7 @@ void loop() {
   //recive data
   String recv_data = Serial.readString();
   //parsing
-    
+  
   
   //servo setting
   servo.set_deg(srvo_deg);
@@ -34,6 +39,30 @@ void loop() {
   //control led
   if(prev_warn_case != warn_case){
     led.set(led_num, warn_case);
+    
+    led_cur_time = millis();
+    led_pre_time = led_cur_time;
+    isledOn = true;
   }
   
+  if(led_spd)
+    led_control();
+  
 }
+
+void led_control(){
+  led_pre_time = led_cur_time;
+  led_cur_time = millis();
+  if((led_cur_time-led_pre_time) >= led_spd * 1000){
+    //switching
+    if(isledOn){   //turn on -> off
+      ledsp_blinkLED(false);
+      isledOn = false;
+    }
+    else{
+      ledsp_blinkLED(true);
+      isledOn = true;
+    }
+  }
+}
+  
