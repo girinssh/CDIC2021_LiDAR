656e91fd113f81aee3627833ba72c85e694f070b
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2021-10-03 22:27:04 +0900

    Arduino Main

--- Arduino/jaemin/ArduinoMain/ArduinoMain.ino
+++ Arduino/jaemin/ArduinoMain/ArduinoMain.ino
@@ -8,7 +8,7 @@ SoftwareSerial subArduino(10, 11);
 SoftwareSerial rasberry(4, 5);
 
 
-#define GPSBAUD 115200
+#define GPSBAUD 9600
 int servo_pin[] = {A0, A1, A2};
 int step_pin[] = {A7, A8, A9, A10, A11, A12};
 int step_speed[] = {1000, 400};
@@ -23,37 +23,62 @@ boolean isLedOn = false;
 String getstr = "";
 String pre_warn_case= "", warn_case;
 
+float serv_deg[3];
+
 void setup() {
   //led, speaker init
   ledsp_setup();
   Serial.begin(9600);
-  //step_moter init
-  //step_moter.stepMmove();
+
   subArduino.begin(9600);
 
   Serial.println("start");
-  
-  while(!Serial.available()){
-  }
 
-  if(Serial.available()){
-    String c = Serial.readStringUntil('\n');
-    for(int i =0; i< c.length(); i++)
-      subArduino.write(c.charAt(i));
+  //rasbarrypi init
+//  while(!Serial.available()){
+//  }
+//  String get_serv_deg;
+//  while(Serial.available())
+//    get_serv_deg = Serial.readStringUntil('\n');
+//  setting_serv_deg(get_serv_deg);
+
+
+  
+//  while(!Serial.available()){
+//  }
+//
+//  if(Serial.available()){
+//    String c = Serial.readStringUntil('\n');
+//    for(int i =0; i< c.length(); i++)
+//      subArduino.write(c.charAt(i));
+//  }
+  String c= "on";
+
+  for(int i =0; i< c.length(); i++)
+    subArduino.write(c.charAt(i));
+  
+  
+  while(!subArduino.available()){
+    
   }
   
   while(subArduino.available()){
     getstr = subArduino.readStringUntil('\n');
   }
+  getstr.trim();
   Serial.println(getstr);
-  
+
+  //send raspi (init)
+//  String info = "success";
+//  for(int i =0; i< info.lengto(); i++)
+//    Serial.write(info.charAt(i));
 }
 
 void loop() {
   
 //  float gps_velocity = gps.getVelocity();
   //send data(gps_velocity (+ imu(roll, pitch)))
-  if(Serial.available() == 0){
+  if(Serial.available()){
     Serial.println();
   }
   
@@ -62,14 +87,31 @@ void loop() {
   }
 
   //recive data
+  
+  
+  
   String recv_data = Serial.readString();
   //parsing
+  recv_data.trim();
+  String neo_info = recv_data.substring(0, 4);
+  String spd_info = recv_data.charAt(4);
+  String led_info = recv_data.substring(5);
+
+  if(neo_info.equals("001"))
+    neo_info = "110000000011";
+  else if(neo_info.equals("010"))
+    neo_info = "001111000000";
+  else if(neo_info.equals("100"))
+    neo_info = "000000111100";
+  else if(neo_info.equals("000"))
+    neo_info = "000000000000";
   
+  
+  int spd = spd_info.toInt();
   
   //servo setting
-//  servo_moter.SerMotorMove(srvo_deg);
-  
-
+//  servo_moter.SerMotorMove(srvo_deg(spd));
+  String warn_case = "led" + neo_info + "spd" + spd_info + "kind" + led_info;
   
   //warn_case = Serial.readString();
  // warn_case = "led101010101010spd1kind1010";
@@ -92,6 +134,20 @@ void loop() {
     led_control();
     
 }
+
+void setting_serv_deg(String info){
+  int index;
+  index = info.indexOf(",");
+  serv_deg[0] = info.substring(0, index).toFloat();
+  info = info.substring(index +2);
+
+  index = info.indexOf(",");
+  serv_deg[1] = info.substring(0, index).toFloat();
+  info = info.substring(index +2);
+
+  serv_deg[2] = info.toFloat();
+}
+
 
 void led_control(){
   led_cur_time = millis();
