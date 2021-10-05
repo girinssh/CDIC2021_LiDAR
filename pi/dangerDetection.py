9867f78d7a54ed7f5956e67b9b841a62a179d34b
girinssh <tngus5851@gmail.com>
2021-10-05 23:17:54 +0900

    LSM DEBUG

--- pi/dangerDetection.py
+++ pi/dangerDetection.py
@@ -233,7 +233,9 @@ class dangerDetection:
     def estimate(cls, POS, XPOS, YPOS, H):
         _, inlier, outlier = cls.RANSAC(POS, XPOS, YPOS, H)
         param = cls.LSM(POS, inlier, XPOS, YPOS, H)
-        _, pictoPit, pictoRol, ledPit, ledRol = cls.estiSlope(POS, cls.udSlope(POS, param)[1], cls.lrSlope(POS, param)[1])
+        _, ud = cls.udSlope(POS, param)
+        _, lr = cls.lrSlope(POS, param)
+        _, pictoPit, pictoRol, ledPit, ledRol = cls.estiSlope(POS, ud , lr)
         
         _, obspicto, obsled = cls.Obstacle(POS, outlier, XPOS, H)
         return POS, cls.finalPictoLed(pictoRol, obspicto, ledPit, ledRol, obsled)
