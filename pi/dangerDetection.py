7357c433ada92fa0a68f111c19f96989c4ec2a24
girinssh <tngus5851@gmail.com>
2021-10-05 22:38:07 +0900

    pos centered -> param centered

--- pi/dangerDetection.py
+++ pi/dangerDetection.py
@@ -33,7 +33,7 @@ class dangerDetection:
             
             
             param = np.array([
-                sum([p[j][k] * (p[j-2][k-2] - p[j-1][k-2]) for j in range(3)]) for k in indices[-2]])
+                sum([p[j][k] * (p[j-2][k-2] - p[j-1][k-2]) for j in range(3)]) for k in indices[-2:1:1]])
             
             # np.append(param, -sum([p[j][0] * (p[j-2][1]*p[j-1][2] - p[j-2][2]*p[j-1][1]) for j in range(3)]))
             
